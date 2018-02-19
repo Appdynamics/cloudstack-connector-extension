fff5cc746ad2c92a987edcfffc7d0054e8faac3c
Bhuvnesh Kumar <raghur171@gmail.com>
2018-02-19 14:21:44 -0800

    Updated Licenses

--- src/java/com/appdynamics/connectors.cloudstack/CloudStackConnector.java
+++ src/java/com/appdynamics/connectors.cloudstack/CloudStackConnector.java
@@ -1,3 +1,11 @@
+/*
+ *   Copyright 2018. AppDynamics LLC and its affiliates.
+ *   All Rights Reserved.
+ *   This is unpublished proprietary source code of AppDynamics LLC and its affiliates.
+ *   The copyright notice above does not evidence any actual or intended publication of such source code.
+ *
+ */
+
 package com.appdynamics.connectors.cloudstack;
 
 import static com.singularity.ee.controller.KAppServerConstants.CONTROLLER_SERVICES_HOST_NAME_PROPERTY_KEY;
